3c3411f112bb94e8468f746bb373cc1d0b836d5c
Zeel Rabadiya <zeelrabadiya198@gmail.com>
2024-01-20 21:47:54 +0530

    Initialize Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import ThemeModeToggle from './components/theme-mode-toggle'
+
+export default function App() {
+  return (
+    <div>
+      <h1>2331</h1>
+      <Button variant='outline'>Button</Button>
+      <div>
+        <ThemeModeToggle />
+      </div>
+    </div>
+  )
+}
